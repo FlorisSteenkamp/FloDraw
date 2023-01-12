2acbf452e748a208c5e13030a1881e52176c4e11
Floris Steenkamp <steenkampfloris@gmail.com>
2023-01-12 12:07:17 -0700

    changed let to const everywhere

--- src/draw/cubic-bezier.ts
+++ src/draw/cubic-bezier.ts
@@ -9,14 +9,14 @@ function cubicBezier(
         class_ = DEFAULT_CLASS,
         delay? : number) {
 
-    let [[x0,y0],[x1,y1],[x2,y2],[x3,y3]] = bezier;
+    const [[x0,y0],[x1,y1],[x2,y2],[x3,y3]] = bezier;
 
     if (x0 === x3 && x1 === x3 && x2 === x3 &&
         y0 === y3 && y1 === y3 && y2 === y3) {
         return crossHair(g, [x0,y0], class_, 0.2, delay);
     } 
 
-    let $path = document.createElementNS(XMLNS, 'path');
+    const $path = document.createElementNS(XMLNS, 'path');
     $path.setAttributeNS(
         null, 
         "d", 
